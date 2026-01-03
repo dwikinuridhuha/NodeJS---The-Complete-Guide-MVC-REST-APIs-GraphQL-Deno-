947e3fdf52a87982e9f6068fae48178975b49c43
Dwiki Nuri Dhuha <dwiki.nuridhuha@gmail.com>
2026-01-03 20:13:09 +0700

    31. sending response

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ import { createServer } from "http";
 
 const server = createServer(function (req, res) {
   const { headers, url, method } = req;
-  console.log({ headers, url, method });
+  res.setHeader("Content-Type", "text/html");
+  res.write("<!DOCTYPE html>");
+  res.write("<html lang='en'>");
+  res.write("<head><title>Hello</title></head>");
+  res.write("<body><h1>Hello World!</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3000);
