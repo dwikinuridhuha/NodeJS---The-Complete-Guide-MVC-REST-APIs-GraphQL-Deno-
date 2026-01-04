cf9f6fcae5aec44e2085de10fc78d6f521502435
Dwiki Nuri Dhuha <dwiki.nuridhuha@gmail.com>
2026-01-04 20:25:48 +0700

    33. routing request

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ import { createServer } from "http";
 
 const server = createServer(function (req, res) {
   const { headers, url, method } = req;
+
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<!DOCTYPE html>");
+    res.write("<html lang='en'>");
+    res.write("<head><title>Hello</title></head>");
+    res.write(
+      "<body><form action='/message' method='post'><input type='text' name='message'></input><button type='submit'>Send</button></form></body>",
+    );
+    res.write("</html>");
+    res.end();
+    return;
+  }
+
   res.setHeader("Content-Type", "text/html");
   res.write("<!DOCTYPE html>");
   res.write("<html lang='en'>");
